5e719b8eed7ee0f925e28357dcc6fd79cee11d14
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-01-02 08:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const text = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const body = document.querySelector("body");
+
+async function delayFn(){
+	let value = await parseInt(delay.value)*1000;
+	setTimeout(function name() {
+		body.innerHTML = `<h1>${text.value}</h1>`;
+	},value);
+}
+
+
+document.getElementById("btn").addEventListener("click",()=>{
+	delayFn();
+});
